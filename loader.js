f062ad83884f5ff9066b8e267afd29a6eb139d3c
Renaud Stauber <stauberr@dewv.edu>
2018-09-15 11:52:47 +0000

    add template files with empty js

---
+++ loader.js
@@ -0,0 +1,10 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("grass.png");
+game.preloadImage("block1.png");
+game.preloadImage("block2.png");
+game.preloadImage("block3.png");
+game.preloadImage("castle.png");
+game.preloadImage("wall.png");
+game.preloadImage("ann.png", 48, 48);
+game.preloadImage("ball.png", 48, 48);
